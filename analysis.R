3ec30e4c9da41533ee38ba21e2101f49db704f94
jacob-gg <jrg2xq@virginia.edu>
2021-06-26 16:51:25 -0400

    add wikipedia scrape for black cherry trees

--- analysis.R
+++ analysis.R
@@ -1,10 +1,24 @@
-# tree data analysis
-# diameter, height and volume of timber in 31 felled black cherry trees.
+# Tree data analysis
+# `trees` contains the diameter, height and volume of 31 felled black cherry trees
 
-# packages
+# Packages
 library(ggplot2)
+library(rvest)
+library(stringi)
 
-# volume v. girth
+# First: What are these things?
+black_cherry_wiki <- read_html('https://en.wikipedia.org/wiki/Prunus_serotina') %>% 
+  html_elements('p') %>%
+  html_text() %>% 
+  .[min(which(nchar(.) > 50))] %>% 
+  gsub('(\\[)(\\d+)(\\])', '', .)
+black_cherry_wiki
+
+#####################
+# Assorted analyses #
+#####################
+
+# Volume v. girth
 data("trees")
 summary(trees)
 pairs(trees)
@@ -12,13 +26,13 @@ pairs(trees)
 pairs(trees, panel = panel.smooth, main = "trees data")
 plot(Volume ~ Girth, data = trees, log = "xy")
 
-# height v. girth
+# Height v. girth
 ggplot(trees[order(trees$Girth), ], aes(x = Girth, y = Height)) +
   geom_point() +
   geom_smooth(method = 'loess', color = 'forestgreen') +
   ggtitle('Height versus Girth')
 
-# bootstrap girth coefficient in volume ~ girth regression
+# Bootstrap girth coefficient in volume ~ girth regression
 mod <- lm(Volume ~ Girth, data = trees)
 set.seed(12)
 b <- 1000
@@ -31,15 +45,15 @@ for (i in 1:b) {
   coefs[i, ] <- temp_mod$coefficients
 }
 colnames(coefs) <- c('Intercept', 'Girth')
-# bootstrapped girth coefficient estimate
+# Bootstrapped girth coefficient estimate
 mean(coefs[, 2])
 # SE of bootstrapped girth coefficient estimate
 sd(coefs[, 2])
 # 95% percentile CI for bootstrapped girth coefficient
 quantile(coefs[, 2], c(.025, .975))
-# estimate degree of bias in bootstrapped girth coefficients
+# Estimate degree of bias in bootstrapped girth coefficients
 abs((sum(coefs[, 2] > coef(mod)[2]) / b) - 0.5)
 
-# histogram of bootstrapped coefficients
-hist(coefs[,'Girth'], main = "DistrIBution of Bootstrapped COeFfiCients",
+# Histogram of bootstrapped coefficients
+hist(coefs[,'Girth'], main = "Bootstrap-coefficient Distribution",
      xlab = "Bootstrap Girth")
